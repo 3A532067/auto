f4463dc52aa2c7c77bcf44b24664dd60eca30efa
3A532067 <s3a532067@student.ncut.edu.tw>
2018-10-10 17:41:23 +0800

    加入 Lara.php

--- src/Demo/Hello/Lara.php
+++ src/Demo/Hello/Lara.php
@@ -1,18 +1,19 @@
 <?php
-namespace Demo;
+namespace Demo\Hello;
 
-class HelloWorld
+class Lara
 {
     function __construct()
     {
-        echo 'Hello World ~'.PHP_EOL;
+        echo 'Hello Lara~'.PHP_EOL;
     }
 }
+
 
 
 /**
  * Created by PhpStorm.
  * User: User
  * Date: 2018/10/10
- * Time: 下午 05:33
+ * Time: 下午 05:40
  */
