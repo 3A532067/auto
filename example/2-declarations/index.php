7afe4738bfc0645510e7adac01a2e5cc27755c4e
3A532067 <s3a532067@student.ncut.edu.tw>
2018-10-10 23:14:28 +0800

    增加 declarations

---
+++ example/2-declarations/index.php
@@ -0,0 +1,23 @@
+<?php
+
+     $pageTitle = 'Hello, world';
+     $pageContent = 'Long long time ago...';
+ ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>PHP 宣告與輸出 | 2018台中市政府教育局 勤益大學 Laravel 工作坊 </title>
+</head>
+<body>
+     <h1><?php echo $pageTitle ?></h1>
+     <p><?=$pageContent?></p>
+</body>>
+</html>
+
+/**
+ * Created by PhpStorm.
+ * User: User
+ * Date: 2018/10/10
+ * Time: 下午 11:00
+ */
