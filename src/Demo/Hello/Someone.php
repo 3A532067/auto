b8c330b28b05449fda75bcf207dae99db21c01e9
3A532067 <s3a532067@student.ncut.edu.tw>
2018-10-10 17:42:58 +0800

    加入 Someone.php

---
+++ src/Demo/Hello/Someone.php
@@ -0,0 +1,17 @@
+<?php
+namespace Demo\Hello;
+
+class Someone
+{
+    function __construct($name)
+    {
+        echo 'Hello '.$name.PHP_EOL;
+    }
+}
+
+/**
+ * Created by PhpStorm.
+ * User: User
+ * Date: 2018/10/10
+ * Time: 下午 05:42
+ */
