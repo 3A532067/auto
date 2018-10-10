4b6229d2f62da9e3d8816c4ea86ee42045bc8633
3A532067 <s3a532067@student.ncut.edu.tw>
2018-10-10 17:36:45 +0800

    加入 HelloWorld.php & 修改 .gitignore

---
+++ src/Demo/HelloWorld.php
@@ -0,0 +1,21 @@
+<?php
+namespace Demo;
+
+class HelloWorld
+{
+    function __construct()
+    {
+        echo 'Hello World ~'.PHP_EOL;
+    }
+}
+
+
+
+
+
+/**
+ * Created by PhpStorm.
+ * User: User
+ * Date: 2018/10/10
+ * Time: 下午 05:33
+ */
