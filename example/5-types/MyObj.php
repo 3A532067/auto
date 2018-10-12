958a493610d4cf82ab282c402234cf04115de213
3A532067 <s3a532067@student.ncut.edu.tw>
2018-10-12 23:29:45 +0800

    新增 types

---
+++ example/5-types/MyObj.php
@@ -0,0 +1,17 @@
+<?php
+
+class MyObj
+{
+    public function method()
+    {
+        echo 'call method in MyObj'."\n";
+    }
+}
+
+
+/**
+ * Created by PhpStorm.
+ * User: User
+ * Date: 2018/10/12
+ * Time: 下午 11:27
+ */
