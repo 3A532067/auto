6cb3976f7f2d21928cf9e7f9b541fc97551ce08b
3A532067 <s3a532067@student.ncut.edu.tw>
2018-10-12 23:17:00 +0800

    新增 variables

---
+++ example/4-variables/index.php
@@ -0,0 +1,21 @@
+<?php
+
+$var = 'Bob';   //合法，小寫開頭
+$Var = 'Joe';   //合法，大寫開頭
+$_4site = 'hello world';   //合法，底線開頭
+
+//$4site = 'helli,world'; //不合法，數字開頭
+/*
+ *
+Parse error: syntax error, unexpected '4' (T_LNUMBER), expecting variable (T_VARIABLE) or '$' in
+ */
+
+<?>
+
+
+/**
+ * Created by PhpStorm.
+ * User: User
+ * Date: 2018/10/12
+ * Time: 下午 11:10
+ */
