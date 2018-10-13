61618c75c172589287b89adfb57df195202cb6ca
3A532067 <s3a532067@student.ncut.edu.tw>
2018-10-13 23:32:59 +0800

    新增 conditions

---
+++ example/10-conditions/index.php
@@ -0,0 +1,23 @@
+<?php
+
+echo '<pre>';
+
+$x = 21;
+
+if ($x < 20) {
+    echo '小於 20';
+} elseif ($x > 20) {
+    echo '大於 20';
+} else {
+    echo '等於 20';
+}
+
+echo '</pre>';
+
+?>
+
+ * Created by PhpStorm.
+ * User: User
+ * Date: 2018/10/13
+ * Time: 下午 11:31
+ */
