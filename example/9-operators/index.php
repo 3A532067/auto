3fee6d3958d98edc0934d6a32864ece76acd75b8
3A532067 <s3a532067@student.ncut.edu.tw>
2018-10-13 23:31:10 +0800

    新增 operators

---
+++ example/9-operators/index.php
@@ -0,0 +1,68 @@
+<?php
+
+echo '<pre>';
+
+// part 1
+$x = 10;
+$y = 2;
+
+$result = $x + $y;
+echo $result."\n";
+
+$result = $x - $y;
+echo $result."\n";
+
+$result = $x * $y;
+echo $result."\n";
+
+$result = $x / $y;
+echo $result."\n";
+
+echo "\n\n";
+
+
+// part 2
+$x = 20;
+
+$x += 2;
+echo $x."\n";
+
+$x -= 6;
+echo $x."\n";
+
+$x *= 5;
+echo $x."\n";
+
+$x /= 2;
+echo $x."\n";
+
+$x .= '-files.txt';
+echo $x."\n";
+
+echo "\n\n";
+
+
+// part 3
+$x = 20;
+
+$x++;
+echo $x."\n";
+
+$x--;
+echo $x."\n";
+
+
+echo '</pre>';
+
+?>
+
+
+
+
+
+/**
+ * Created by PhpStorm.
+ * User: User
+ * Date: 2018/10/13
+ * Time: 下午 11:30
+ */
