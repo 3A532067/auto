267736a889a1bbede1295855734ab8b91581e38b
3A532067 <s3a532067@student.ncut.edu.tw>
2018-10-13 23:42:21 +0800

    新增 comparisions

---
+++ example/11-comparisions/index.php
@@ -0,0 +1,43 @@
+<?php
+
+
+echo '<pre>';
+
+// part 1
+$x = 20;
+var_dump($x > 20);
+var_dump($x >= 20);
+var_dump($x < 20);
+var_dump($x <= 20);
+
+echo "\n\n";
+
+var_dump($x == 20);
+var_dump($x != 20);
+
+echo "\n\n";
+
+// part 2
+$int = 22;
+$bool = false;
+
+var_dump(!$bool);
+
+echo "\n\n";
+
+var_dump($int > 0 && $int < 50);
+var_dump($int > 0 || $int < 20);
+
+echo "\n\n";
+
+echo '</pre>';
+
+?>
+
+
+/**
+ * Created by PhpStorm.
+ * User: User
+ * Date: 2018/10/13
+ * Time: 下午 11:39
+ */
