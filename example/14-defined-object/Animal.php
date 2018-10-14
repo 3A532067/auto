d6276d5865df2e49a0cd3f044024feae69657abe
3A532067 <s3a532067@student.ncut.edu.tw>
2018-10-14 16:43:27 +0800

    新增 defined-object

---
+++ example/14-defined-object/Animal.php
@@ -0,0 +1,29 @@
+<?php
+
+class Animal
+{
+    public $name;
+    protected $axis;
+
+    public function __construct($name)
+    {
+        $this->name = $name;
+        $this->axis = ['x' => 0, 'y' => 0];
+    }
+
+    public function move($x, $y)
+    {
+        $this->axis['x'] += $x;
+        $this->axis['y'] += $y;
+
+        return $this->axis;
+    }
+}
+
+
+/**
+ * Created by PhpStorm.
+ * User: User
+ * Date: 2018/10/14
+ * Time: 下午 04:09
+ */
---
+++ example/14-defined-object/index.php
@@ -0,0 +1,15 @@
+<?php
+
+require 'Animal.php';
+
+$animal = new Animal('Lucky');
+
+echo '<pre>';
+var_dump($animal);
+echo '</pre>';
+/**
+ * Created by PhpStorm.
+ * User: User
+ * Date: 2018/10/14
+ * Time: 下午 04:09
+ */
